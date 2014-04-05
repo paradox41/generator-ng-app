07e5245c47d069bb3d5bc9d743ff1a1fd216dd5d
Will <will@coefficientinc.com>
2014-04-05 17:00:57 -0400

    Add config

---
+++ app/templates/config.js
@@ -0,0 +1,39 @@
+require.config({
+    baseUrl: '',
+    deps: ['angular', 'app'],
+    paths: {
+        'common.api': 'common/api',
+
+        // AngularJS Modules
+        'angular': 'bower_components/angular/angular',
+        'nvd3ChartDirectives': 'bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives',
+        'ui.bootstrap': 'bower_components/angular-bootstrap/ui-bootstrap-tpls',
+        'ui.router': 'bower_components/angular-ui-router/release/angular-ui-router',
+
+        // Utility libraries
+        'lodash': 'bower_components/lodash/dist/lodash',
+        'moment': 'bower_components/moment/moment',
+
+        // charting libraries
+        'd3': 'bower_components/d3/d3',
+        'nv': 'bower_components/nvd3/nv.d3'
+    },
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'lodash': {
+            exports: '_'
+        },
+        'd3': {
+            exports: 'd3'
+        },
+        'nv': {
+            exports: 'nv',
+            deps: ['d3']
+        },
+        'nvd3ChartDirectives': ['angular', 'd3', 'nv'],
+        'ui.router': ['angular'],
+        'ui.bootstrap': ['angular']
+    }
+});
