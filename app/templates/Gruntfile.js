69d3253b8d2164918e7805b3f3749651af51c342
Will <will@coefficientinc.com>
2014-04-05 17:00:48 -0400

    Add gruntfile

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var _ = require('lodash');
+
+// load config for grunt
+var loadConfig = function (path) {
+    var glob = require('glob');
+    var object = {};
+    var key;
+
+    glob.sync('*', { cwd: path }).forEach(function (option) {
+        key = option.replace(/\.js$/,'');
+        object[key] = require(path + option);
+    });
+
+    return object;
+};
+
+module.exports = function(grunt) {
+    // var _ = require('lodash');
+    // https://github.com/sindresorhus/time-grunt
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    var config = _.assign({
+        pkg: require('./package')
+    }, loadConfig('./tasks/options/'));
+
+    grunt.initConfig(config);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'sass:dev',
+        'concurrent:dev'
+    ]);
+
+    grunt.registerTask('test', [
+        'karma:unit'
+    ]);
+
+    grunt.registerTask('reporting', [
+        'plato:report'
+    ]);
+
+    grunt.registerTask('build', [
+        'jshint',
+        'clean:build',
+        'copy:build',
+        'requirejs:build',
+        'sass:build'
+    ]);
+};
